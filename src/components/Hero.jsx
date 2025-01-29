7133ce01263d9c6a1479a3cb2c50715aa3ff4a93
dhavalthakur11 <dhavalthakor88551@gmail.com>
2025-01-29 14:09:35 +0530

    add a 3d model in website and complete hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,10 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
+
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className={`relative w-full h-screen mx-auto`}>
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
@@ -24,6 +25,24 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y:[0,24,0], 
+              }}
+              transition={{
+                duration:1.5,
+                repeat:Infinity,
+                repeatType:"loop"
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
